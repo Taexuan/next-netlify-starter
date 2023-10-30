fb9c4ed752a27d522f260ccc7f3da25a4d2452ac
Kevin Uyen Pfister Nguyen Xuan <kevin_uyen.pfister@mailbox.tu-dresden.de>
2023-10-30 18:34:31 +0100

    throwback

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { Handler } from "@netlify/functions";
-import { withPlanetscale } from "@netlify/planetscale";
-
-export const handler = withPlanetscale(async (event, context) => {
-  const {
-    planetscale: { connection }
-  } = context
-
-  const { body } = event
-
-  if (!body) {
-    return {
-      statusCode: 400,
-      body: "Missing body"
-    }
-  }
-
-  const { email, name } = JSON.parse(body)
-
-  await connection.execute("INSERT INTO users (email, name) VALUES (?, ?)", [
-    email,
-    name
-  ])
-
-  return {
-    statusCode: 201
-  }
-})
 
 export default function Home() {
   return (
