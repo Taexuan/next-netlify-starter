adbcff115ce45956ba4a126351383aea6ff8c2c2
Kevin Uyen Pfister Nguyen Xuan <kevin_uyen.pfister@mailbox.tu-dresden.de>
2023-10-30 23:42:15 +0100

    roll character

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,56 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [imageSource, setImageSource] = useState('https://cdn.myanimelist.net/images/characters/4/201257.jpg');
+  const [charName, setName] = useState('Haruka Niimi');
+  const [anime, setAnime] = useState('Test');
+
+  const callAPI = async () => {
+		try {
+			const res = await fetch(
+				`https://api.jikan.moe/v4/random/characters`
+			);
+			const data = await res.json();
+			console.log(data.data);
+      setImageSource(data.data.images.jpg.image_url);
+      setName(data.data.name);
+      animeById(data.data.mal_id);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+  const animeById = async (id) => {
+		try {
+			const res = await fetch(
+				`https://api.jikan.moe/v4/characters/${id}/anime`
+			);
+			const data = await res.json();
+			console.log(data.data);
+      setAnime(data.data[0].anime.title);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Waifu Gatcha</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <button onClick={callAPI}>Make API call</button>
+        <p>{charName} - {anime}</p>
+        <div className='image'>
+        <img src={imageSource} alt="Picture of the author"/>
+        </div>
       </main>
 
       <Footer />
